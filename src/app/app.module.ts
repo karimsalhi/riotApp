100f4f7d7440c1d206b2e00839ee9c308633743c
Tony Blard <tony.blard@epita.fr>
2022-10-21 12:30:05 +0200

    Search Component Create class SummonerName Refactor search in component TODO refactor SummonerName class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SummonerService } from './services/summoner.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SearchComponent } from './components/search/search.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [SummonerService],
   bootstrap: [AppComponent]
